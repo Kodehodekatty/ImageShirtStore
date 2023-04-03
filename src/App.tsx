b0cbe21e52a9652f53806c6518d448f6d696a672
Kathrine <kathrine.kodehode@gmail.com>
2023-04-03 16:48:10 +0200

    first-commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { useState, useEffect, FunctionComponent } from "react";
+import "././styles/App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <main className="main">
+        <h1>Space X</h1>
+      </main>
+    </div>
+  );
+}
+
+export default App;
